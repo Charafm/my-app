c5a295eca701ee85955ffdcbd903ae618afc3b8b
Charaf Eddine Moujahid <charafeddine.mojahid@gmail.com>
2023-05-02 17:39:35 +0100

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,11 +1,9 @@
 const Pool = require("pg").Pool;
-
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
+  user: process.env.DB_USER || "moujahidc",
+  password: process.env.DB_PASSWORD || "YahyaMjhd2001",
   host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "postgres",
+  port: process.env.DB_PORT || 3001,
+  database: process.env.DB_NAME || "moujahidc",
 });
-
 export default pool;
